78397bf1485e01ad76694e52306c6ee8c8dc2372
Gerardo Roger <gerovi90@hotmail.com>
2021-06-02 20:04:42 +0200

    Add cache to API

--- src/main/java/com/atsistemas/superhero/models/service/implementation/SuperheroServiceImpl.java
+++ src/main/java/com/atsistemas/superhero/models/service/implementation/SuperheroServiceImpl.java
@@ -4,6 +4,7 @@ import com.atsistemas.superhero.models.dao.ISuperheroDao;
 import com.atsistemas.superhero.models.entity.Superhero;
 import com.atsistemas.superhero.models.service.ISuperheroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.*;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +16,13 @@ public class SuperheroServiceImpl implements ISuperheroService {
     private ISuperheroDao superheroDao;
 
     @Override
+    @Cacheable(value = "superheroes")
     public List<Superhero> findAll() {
         return (List<Superhero>) superheroDao.findAll();
     }
 
     @Override
+    @Cacheable(value = "superheroe", key = "#id")
     public Superhero findOne(int id) {
         return superheroDao.findById(id).orElse(null);
     }
@@ -30,11 +33,25 @@ public class SuperheroServiceImpl implements ISuperheroService {
     }
 
     @Override
+    @Caching(
+            put = {
+                    @CachePut(value = "superheroe",  key = "#superhero.id")
+            },
+            evict = {
+                    @CacheEvict(value = "superheroes", allEntries = true),
+            }
+    )
     public Superhero update(Superhero superhero) {
         return superheroDao.save(superhero);
     }
 
     @Override
+    @Caching(
+            evict = {
+                @CacheEvict(value = "superheroes", allEntries = true),
+                @CacheEvict(value = "superheroe", key = "#id")
+            }
+    )
     public void delete(int id) {
         superheroDao.deleteById(id);
     }
